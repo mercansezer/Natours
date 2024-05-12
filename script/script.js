a0bd47e1ae7bf32ffd7a5179de93ec9cab6d07e9
Sezer Mercan <sezermercan12@hotmail.com>
2024-05-12 16:28:20 +0300

    Responsive design finished

--- script/script.js
+++ script/script.js
@@ -3,7 +3,6 @@ const popupClose = document.querySelector(".popup__close");
 const popupOpen = document.querySelectorAll(".popup__open");
 const popupSection = document.querySelector(".popup");
 const popupContent = document.querySelector(".popup__content");
-console.log(popupOpen);
 
 popupClose.addEventListener("click", function (e) {
   popupSection.classList.add("hidden");

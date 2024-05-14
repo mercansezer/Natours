5f695e755ce74af54d392b096fa7722dff50352e
Sezer Mercan <sezermercan12@hotmail.com>
2024-05-15 01:38:17 +0300

    Optimization finished.

--- script/script.js
+++ script/script.js
@@ -3,6 +3,11 @@ const popupClose = document.querySelector(".popup__close");
 const popupOpen = document.querySelectorAll(".popup__open");
 const popupSection = document.querySelector(".popup");
 const popupContent = document.querySelector(".popup__content");
+const popup = document.querySelector(".popup");
+const allLinks = document.querySelectorAll("a:link");
+const checkBox = document.querySelector(".navigation__checkbox");
+const navBackground = document.querySelector(".navigation__background");
+const nav = document.querySelector(".navigation__nav");
 
 popupClose.addEventListener("click", function (e) {
   popupSection.classList.add("hidden");
